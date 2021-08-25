44dd4a46c41974edb9093635e4d9a3d000912ceb
Chris Van Lanen-Wanek <cvanlane@epic.com>
2021-08-25 15:26:50 -0500

    Added isPrime and isSumOfSquares functions

--- Int.ts
+++ Int.ts
@@ -1,5 +1,6 @@
 // Integer related functions
 
+// Needs testing
 export default class Int {
     public static isNonNegative = (n: number): boolean => {
         return n >= 0;
@@ -9,6 +10,24 @@ export default class Int {
     }
     public static isNonNegativeInt = (n: number): boolean => {
         return Int.isNonNegative(n) && Int.isInt(n);
+    }
+    public static isPrime = (n: number): boolean => {
+        for (let i = 2; i * i <= n; i++) {
+            if (n % i === 0) return false;
+        }
+        return true;
+    }
+    public static isSumOfSquares = (n: number): boolean => {
+        let count: number;
+        for (let i = 2; i * i <= n; i++) {
+            count = 0;
+            while (n % i === 0) {
+                n /= i;
+                count++;
+            }
+            if (i % 4 === 3 && count % 2 !== 0) return false;
+        }
+        return n % 4 !== 0;
     }
     // Needs to be tested
     public static factorial = (n: number): number => {
